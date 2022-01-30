4975e977f1bbc0140a24ec5309a4587a4d39b9ca
fcesc-code <bpfran@gmail.com>
2022-01-30 15:51:58 +0100

    config / update route apis to match the frontend

--- backend/src/courses/controllers/courses.controller.ts
+++ backend/src/courses/controllers/courses.controller.ts
@@ -2,14 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 import { Course } from '../../../../shared/course';
 import { findAllCourses } from '../../../mockdata/db-data';
 
-@Controller('courses')
+@Controller('api/courses')
 export class CoursesController {
-  @Get('api/hello-world')
+  @Get('hello-world')
   async getHome(): Promise<string> {
     return 'Hello world!';
   }
 
-  @Get('api')
+  @Get('')
   async findAllCourses(): Promise<Course[]> {
     return findAllCourses();
   }
