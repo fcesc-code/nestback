9c96188c9947dd846f07ecf7501fc316d6ec60c2
fcesc-code <bpfran@gmail.com>
2022-03-05 08:12:04 +0100

    safecopy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['./../enviroments/.development.env'],
+    }),
+    CoursesModule,
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.group('Environment Variables');
+  console.log(process.env);
+  console.groupEnd();
   const app = await NestFactory.create(AppModule);
+
   await app.listen(9000);
 }
 
